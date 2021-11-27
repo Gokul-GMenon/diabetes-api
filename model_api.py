37d476babca430b9153263d593099cd14f96397f
Gokul G Menon <gokulgovindmenon@gmail.com>
2021-11-27 22:25:33 +0530

    edited for tutored push2

--- model_api.py
+++ model_api.py
@@ -31,4 +31,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True) #, host='0.0.0.0', port=os.getenv('PORT'))
+
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port) #, host='0.0.0.0', port=os.getenv('PORT'))
