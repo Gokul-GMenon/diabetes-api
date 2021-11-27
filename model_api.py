ad69f1d5c1137b3d67ba1cbcf309bbdf689fdd16
Gokul G Menon <gokulgovindmenon@gmail.com>
2021-11-27 18:27:42 +0530

    files added

---
+++ model_api.py
@@ -0,0 +1,33 @@
+import keras
+import numpy as np
+from flask import Flask, jsonify, request
+
+def predict(model, data):
+
+    data = np.array([data], dtype=float)
+
+    prediction = 1 if model.predict(data)>0.5 else 0
+
+    return {'result':prediction}
+
+
+model = keras.models.load_model('diabetes.h5')
+
+app = Flask(__name__)
+
+@app.route('/predict/', methods=['POST'])
+def load_preddict():
+
+    data = request.get_json(force=True)
+    print('\n', data,'\n')
+    prediction = predict(model, data['input'])
+   
+    return jsonify(prediction)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Machine Learning Inference'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
